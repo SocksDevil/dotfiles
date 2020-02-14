f9a2015d7f10d49b04a22d56a231a50230fcb473
Andre Rocha <andrefmrocha@live.com.pt>
2020-02-14 23:02:42 +0000

    Added new polybar design

--- .config/polybar/i3wmthemer_bar_launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env sh
-
-pkill polybar
-
-sleep 1;
-
-polybar neon &
-polybar left_bar &
-polybar hdmi_bar &
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
 # Terminate already running bar instances
 killall -q polybar
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2  
-nohup polybar topbar > /dev/null 2>&1 &
-nohup polybar bottombar > /dev/null 2>&1 &
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar main &
+polybar secondary &
